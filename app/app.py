e2acb2e78458845fd0d03e5414d9c2fb8cddab59
ErikHuuskonen <127846692+ErikHuuskonen@users.noreply.github.com>
2024-02-19 10:25:21 +0200

    structure changes

--- app/app.py
+++ app/app.py
@@ -8,18 +8,16 @@ from datetime import datetime
 from werkzeug.datastructures import FileStorage
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
-
-
 from app.utils.help_functions import HelpFunctions
 
 app = Flask(__name__)
 app.secret_key = getenv('SECRET_KEY')
 
 #For local
-#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
 #For deployment
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 
 db = SQLAlchemy(app)
 
--- app/models.py
+++
@@ -1,7 +0,0 @@
-from src import db
-from sqlalchemy.dialects.postgresql import JSON
-
-class User(db.Model):
-    __tablename__ = 'userinfo'
-
-    
